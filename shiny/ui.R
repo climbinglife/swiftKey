17c7728984fd4d4b48d95a5d5b93a7b0b956fa5b
Climbinglife <jincheng@buffalo.edu>
2016-07-04 12:28:32 -0400

    adjust ui

--- shiny/ui.R
+++ shiny/ui.R
@@ -4,6 +4,7 @@ shinyUI(fluidPage(
   headerPanel("Prediction of next word based on n-grams model"),
   fluidRow(
       column(width=8, 
+          h3('Input text for next word prediction'),
           textInput("text", "Enter the text", "What is the weather"),
           actionButton("predict", "Predict the next word")
       )
@@ -14,5 +15,11 @@ shinyUI(fluidPage(
         h4('Next word is:'),
         h4(textOutput('result'))
     )
+  ),
+  fluidRow(
+      column(width=8,
+             HTML('<hr height="30">'),
+             h5(a("About this app", href="https://github.com/climbinglife/swiftKey"))
+      )
   )
 ))
